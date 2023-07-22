e9bd1fcc68c028a06954609d8f1275d0f317fc24
Siqm <rafaelsmello01@gmail.com>
2023-07-22 15:09:45 -0300

    [CoinSelector] fix for weird yellow color on text

--- src/components/coinSelector/index.jsx
+++ src/components/coinSelector/index.jsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from "react";
 import styles from './styles.module.scss'
 
+/**
+ * Componente que representa um seletor de moedas.
+ * 
+ * @param {Object} param0 - Um objeto contendo os seguintes atributos:
+ * @param {Array} param0.coinsList - Uma matriz de objetos representando as opções de moedas disponíveis para seleção.
+ * Cada objeto deve ter as seguintes propriedades:
+ * @param {string} param0.coinsList.name - O nome da moeda.
+ * @param {string} param0.coinsList.moeda - O símbolo ou código da moeda.
+ * @param {number} param0.coinsList.index - O índice da moeda na matriz coinsList.
+ * @param {string} param0.defaultCoin - A moeda padrão selecionada inicialmente no seletor. Deve ser o símbolo ou código da moeda.
+ * @param {function} param0.onChange - Uma função de retorno de chamada (callback) a ser invocada quando a moeda selecionada mudar.
+ * A função de callback será chamada com o símbolo ou código da moeda selecionada como seu único argumento.
+ * @returns {JSX.Element} - O componente React que renderiza o seletor de moedas.
+ */
 export default function CoinSelector({ coinsList, defaultCoin, onChange }) {
+    // Corpo da função (lógica do componente) vai aqui.
+    // O componente renderiza o seletor de moedas com base nos parâmetros fornecidos.
 
     function handleChanges(event) {
 

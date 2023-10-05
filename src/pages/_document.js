45ac790b1f91de9d9889136352c3bca7d92e8b4b
Siqm <rafaelsmello01@gmail.com>
2023-10-05 07:34:13 -0300

    [Ad]

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -11,6 +12,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1635772580987396"
+          crossOrigin='anonymous'
+        />
       </body>
     </Html>
   )

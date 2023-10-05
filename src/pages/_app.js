dc35acbe66057c996abda99deea8742f4e97a004
Siqm <rafaelsmello01@gmail.com>
2023-10-05 08:00:04 -0300

    [GA] Fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,47 @@
-import '@/styles/globals.scss'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import '@/styles/globals.scss'
+import { useEffect } from 'react'
+import * as gtag from '../../lib/gtag'
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1635772580987396"
-        crossOrigin='anonymous'
-        async
         strategy="afterInteractive"
-        onError={(e) => console.log('Script failed to load', e)}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Component {...pageProps} />
     </>
   )
 }
+
+export default App

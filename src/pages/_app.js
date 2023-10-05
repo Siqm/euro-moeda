623e157827587effce329e9934b011a645607783
Siqm <rafaelsmello01@gmail.com>
2023-10-05 07:41:07 -0300

    [Ads]

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1635772580987396"
+        crossOrigin='anonymous'
+        async
+        strategy="afterInteractive"
+        onError={(e) => console.log('Script failed to load', e)}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
